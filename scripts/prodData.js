db267df3c91ffadceaba938ed99933767c30e4ab
Enrique <epiquerass@gmail.com>
2017-01-08 01:23:40 -0800

    Finished beta

--- scripts/prodData.js
+++ scripts/prodData.js
@@ -1,8 +1,24 @@
+import mongoose from 'mongoose';
 import path from 'path';
 import slug from 'limax';
+import Schools from '../server/models/schools';
 
 const filename = process.argv[2];
 const filepath = path.join('../data/', filename);
-let data = require(filepath).default;
+let data = require(filepath).default; // eslint-disable-line import/no-dynamic-require
 
-console.log(data);
+mongoose.connect(process.env.MONGO_URL, (error) => {
+  if (error) {
+    console.error('Please make sure Mongodb is installed and running!'); // eslint-disable-line no-console
+    throw error;
+  } else {
+    console.log('Connected to database!'); // eslint-disable-line no-console
+
+    data = data.map(school => (
+      { name: school, slug: slug(school.toLowerCase(), { lowercase: true }) }
+    ));
+
+    // eslint-disable-next-line no-console
+    Schools.create(data, () => console.log('Schools added to schools collection.'));
+  }
+});
