0fe78d11219ae35aaa92cbd38433ce47d645f23b
Enrique <epiquerass@gmail.com>
2017-01-08 01:54:00 -0800

    Fixed port issue

--- server/serverConfig.js
+++ server/serverConfig.js
@@ -1,6 +1,6 @@
 const serverConfig = {
-  mongoUrl: process.env.NODE_ENV === 'production' ? process.env.MONGO_URL : 'mongodb://localhost:27017/rankea-tu-profe',
-  port: process.env.NODE_ENV === 'production' ? process.env.PORT || 3000 : 3000,
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/rankea-tu-profe',
+  port: process.env.PORT || 3000,
 };
 
 export default serverConfig;
